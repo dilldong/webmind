c4902a8451e95a3eefed880d88aa32241a0180d0
Ping <idea.pdware@gmail.com>
2017-04-09 19:03:50 +0800

    no message

--- src/framework/org/mind/framework/port/UploadProperty.java
+++ src/framework/org/mind/framework/port/UploadProperty.java
@@ -2,13 +2,13 @@ package org.mind.framework.port;
 
 /**
  * 上传文件成功后的属性值。
- * 包括前台也面file的name值，上传后的文件名称、文件路径、文件大小。
+ * 包括前段filed的name值，上传后的文件名称、文件路径、文件大小。
  * 
  * @author dongping
  */
 public class UploadProperty {
 	
-	private String pageName;
+	private String filedName;
 	
 	private String fileName;
 	
@@ -18,8 +18,8 @@ public class UploadProperty {
 	
 	private String fileType;
 
-	public UploadProperty(String pageName, String fileName, String filePath, long fileSize, String fileType) {
-		this.pageName = pageName;
+	public UploadProperty(String filedName, String fileName, String filePath, long fileSize, String fileType) {
+		this.filedName = filedName;
 		this.fileName = fileName;
 		this.filePath = filePath;
 		this.fileSize = fileSize;
@@ -45,16 +45,14 @@ public class UploadProperty {
 	public void setFileName(String fileName) {
 		this.fileName = fileName;
 	}
-
-
-	public String getPageName() {
-		return pageName;
+	public String getFiledName() {
+		return filedName;
 	}
 
-	public void setPageName(String pageName) {
-		this.pageName = pageName;
+	public void setFiledName(String filedName) {
+		this.filedName = filedName;
 	}
-	
+
 	public String getFilePath() {
 		return filePath;
 	}
@@ -74,7 +72,7 @@ public class UploadProperty {
 	@Override
 	public String toString() {
 		return new StringBuilder()
-			.append("[pageName]").append(this.pageName)
+			.append("[filedName]").append(this.filedName)
 			.append(";[fileName]").append(this.fileName)
 			.append(";[filePath]").append(this.filePath)
 			.append(";[fileSize]").append(this.fileSize)
