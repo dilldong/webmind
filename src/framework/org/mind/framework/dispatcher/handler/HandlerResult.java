901e5a224c7bf6274c8103d8c12fe1a05470f38f
marcus <idea.pdware@gmail.com>
2023-03-03 11:03:59 +0800

    Update

--- src/framework/org/mind/framework/dispatcher/handler/HandlerResult.java
+++ src/framework/org/mind/framework/dispatcher/handler/HandlerResult.java
@@ -19,20 +19,31 @@ public interface HandlerResult {
 
     String LAST_MODIFIED = "Last-Modified";
 
+    String REQUEST_URL = "URL";
+    String REQUEST_METHOD = "Method";
+    String REQUEST_IP = "Request IP";
+
+    String JSON_METHOD = "JSONRPC Method";
+    String REQUEST_RAW_CONTENT = "Raw Content";
+
     void handleResult(Object result, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException;
 
     static void setRequestAttribute(HttpServletRequest request) {
         JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("URL", HttpUtils.getURL(request, true));
-        jsonObject.addProperty("Method", request.getMethod());
-        jsonObject.addProperty("Request IP", HttpUtils.getRequestIP(request, true));
+        jsonObject.addProperty(REQUEST_URL,
+                HttpUtils.getURL(request, true) +
+                        StringUtils.defaultIfEmpty("?" + request.getQueryString(), StringUtils.EMPTY));
+        jsonObject.addProperty(REQUEST_METHOD, request.getMethod());
+        jsonObject.addProperty(REQUEST_IP, HttpUtils.getRequestIP(request, true));
+
         String str = HttpUtils.getJson(request);
+        jsonObject.addProperty(REQUEST_RAW_CONTENT, StringUtils.defaultIfEmpty(str, StringUtils.EMPTY));
+
         if (StringUtils.isNotEmpty(str) && str.contains("jsonrpc")) {
             str = StringUtils.substringBetween(str, "method", ",");
-            str = str.replaceAll("[\'\":]*", "").trim();
-            jsonObject.addProperty("JsonRPC Method", str);
+            str = str.replaceAll("[\'\":]*", StringUtils.EMPTY).trim();
+            jsonObject.addProperty(JSON_METHOD, str);
         }
-
         request.setAttribute(BaseException.EXCEPTION_REQUEST, jsonObject);
     }
 }
