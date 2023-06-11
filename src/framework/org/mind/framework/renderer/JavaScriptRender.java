75aceea4815eff3789c2ee0f215dd198f490a986
marcus <idea.pdware@gmail.com>
2023-06-11 13:39:15 +0800

    Update

--- src/framework/org/mind/framework/renderer/JavaScriptRender.java
+++ src/framework/org/mind/framework/renderer/JavaScriptRender.java
@@ -1,5 +1,7 @@
 package org.mind.framework.renderer;
 
+import org.mind.framework.util.HttpUtils;
+
 /**
  * render JavaScript.
  *
@@ -8,17 +10,17 @@ package org.mind.framework.renderer;
 public class JavaScriptRender extends TextRender {
 
     public JavaScriptRender() {
-        setContentType(MIME_JAVASCRIPT);
+        setContentType(HttpUtils.MIME_JAVASCRIPT);
     }
 
     public JavaScriptRender(String text) {
         super(text);
-        setContentType(MIME_JAVASCRIPT);
+        setContentType(HttpUtils.MIME_JAVASCRIPT);
     }
 
     public JavaScriptRender(String text, String characterEncoding) {
         super(text, characterEncoding);
-        setContentType(MIME_JAVASCRIPT);
+        setContentType(HttpUtils.MIME_JAVASCRIPT);
     }
 
 }
