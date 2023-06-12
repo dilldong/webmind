3b0411e7915a51614ae71269a18ac0eb3cedac4f
marcus <idea.pdware@gmail.com>
2023-06-12 14:35:38 +0800

    Update

--- src/framework/org/mind/framework/SpringApplication.java
+++ src/framework/org/mind/framework/SpringApplication.java
@@ -1,6 +1,17 @@
 package org.mind.framework;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.log4j.PropertyConfigurator;
 import org.mind.framework.server.WebServer;
+import org.mind.framework.server.WebServerConfig;
+import org.mind.framework.util.ClassUtils;
+import org.mind.framework.util.IOUtils;
+import org.mind.framework.util.JarFileUtils;
+import org.springframework.context.ApplicationContext;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Objects;
 
 /**
  * @author Marcus
@@ -27,6 +38,26 @@ public class SpringApplication {
 
     public static void run(Class<?> mainClass, String[] springLocations, String[] resources, String... args) {
         new SpringApplication(mainClass, springLocations, resources).run(args);
+    }
+
+    public static ApplicationContext runApplication(Class<?> mainClass, String log4j, String[] springLocations) {
+        if(StringUtils.isNotEmpty(log4j)) {
+            URL url = ClassUtils.getResource(mainClass, log4j);
+
+            // jar in jar
+            InputStream log4jInputStream;
+            if (Objects.isNull(url)) {
+                log4jInputStream = JarFileUtils.getJarEntryStream(
+                        WebServerConfig.JAR_IN_CLASSES + IOUtils.DIR_SEPARATOR + log4j);
+            } else
+                log4jInputStream = ClassUtils.getResourceAsStream(mainClass, log4j);
+
+            // load log4j
+            PropertyConfigurator.configure(log4jInputStream);
+        }
+
+        // load spring
+        return ContextSupport.initContextByClassPathFile(springLocations);
     }
 
     private void run(String... args) {
