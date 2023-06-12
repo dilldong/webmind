5a0ed2576dc88d92cd5c2be90c62ea1a968429c9
marcus <idea.pdware@gmail.com>
2023-06-12 13:20:00 +0800

    Update

---
+++ src/framework/org/mind/framework/service/TaskThreadFactory.java
@@ -0,0 +1,38 @@
+package org.mind.framework.service;
+
+import org.apache.tomcat.util.threads.TaskThread;
+
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Simple task thread factory to use to create threads for an executor
+ * implementation.
+ */
+public class TaskThreadFactory implements ThreadFactory {
+    private final ThreadGroup group;
+    private final AtomicInteger threadNumber = new AtomicInteger(1);
+    private final String namePrefix;
+    private final boolean daemon;
+    private final int threadPriority;
+
+    public TaskThreadFactory(String namePrefix, boolean daemon, int priority) {
+        group = Thread.currentThread().getThreadGroup();
+        this.namePrefix = namePrefix;
+        this.daemon = daemon;
+        this.threadPriority = priority;
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        TaskThread t =
+                new TaskThread(
+                        group,
+                        r,
+                        namePrefix + threadNumber.getAndIncrement());
+        t.setDaemon(daemon);
+        t.setPriority(threadPriority);
+        t.setContextClassLoader(getClass().getClassLoader());
+        return t;
+    }
+}
