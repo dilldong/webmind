81bcd94658179ca6aa3c31132b6e1ac86619080f
marcus <idea.pdware@gmail.com>
2023-07-21 12:38:02 +0800

    Update

--- src/framework/org/mind/framework/util/RandomCodeUtil.java
+++ src/framework/org/mind/framework/util/RandomCodeUtil.java
@@ -3,13 +3,41 @@ package org.mind.framework.util;
 import org.apache.commons.lang3.RandomStringUtils;
 
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
 
 /**
  * Random code tools
+ *
  * @author marcus
  * @version 1.1
  */
 public final class RandomCodeUtil {
+
+    private RandomCodeUtil() {
+    }
+
+    private static class Helper {
+        private static final AtomicReference<Integer> RANDOM_REF;
+        private static final Function<Integer, UnaryOperator<Integer>> OPERATOR_FUNCTION;
+
+        static {
+            RANDOM_REF = new AtomicReference<>(-1);
+            OPERATOR_FUNCTION = (bound) -> (prev) -> {
+                // ThreadLocalRandom效率更高，每个线程有一个独立的随机数生成器，用于并发产生随机数，能够解决多个线程发生的竞争争夺
+                ThreadLocalRandom tlRandom = ThreadLocalRandom.current();
+                int innerValue = tlRandom.nextInt(bound);
+                while (innerValue == prev && bound > 1)// should different from last random
+                    innerValue = tlRandom.nextInt(bound);
+                return innerValue;
+            };
+        }
+    }
+
+    public static int nextThreadLocalRandom(int bound) {
+        return Helper.RANDOM_REF.updateAndGet(Helper.OPERATOR_FUNCTION.apply(bound));
+    }
 
     public static int random4Digit() {
         return (int) (1_000 + (9_999 - 1_000) * Math.abs(ThreadLocalRandom.current().nextDouble()));
@@ -34,10 +62,9 @@ public final class RandomCodeUtil {
 
     /**
      * A random string of numbers or characters of the specified length
-     * @param length
+     *
      * @param letters true: contains characters
      * @param numbers true: contains numbers
-     * @return
      */
     public static String getRandomString(int length, boolean letters, boolean numbers) {
         return RandomStringUtils.random(length, letters, numbers);
