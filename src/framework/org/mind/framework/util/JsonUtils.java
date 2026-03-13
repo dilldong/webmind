30417805b8d0dc3614ddb78f8d58af44d275b78b
Marcus <idea.pdware@gmail.com>
2026-03-13 11:00:11 +0800

    Update

--- src/framework/org/mind/framework/util/JsonUtils.java
+++ src/framework/org/mind/framework/util/JsonUtils.java
@@ -10,6 +10,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.ToNumberStrategy;
 import com.google.gson.reflect.TypeToken;
 import org.apache.commons.lang3.StringUtils;
+import org.mind.framework.adapter.InstantTypeAdapter;
 import org.mind.framework.adapter.LocalDateTimeAdapter;
 import org.mind.framework.http.Response;
 import org.slf4j.Logger;
@@ -18,6 +19,7 @@ import org.slf4j.LoggerFactory;
 import java.io.Reader;
 import java.lang.reflect.Field;
 import java.lang.reflect.Type;
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.Collection;
@@ -68,8 +70,9 @@ public class JsonUtils {
     private static class SingletonHolder {
         private static final Gson GSON_INSTANCE =
                 new GsonBuilder()
-                        .setDateFormat(DateUtils.DATE_TIME_PATTERN)
                         .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                        .registerTypeAdapter(Instant.class, new InstantTypeAdapter())
+                        .setDateFormat(DateUtils.DATE_TIME_PATTERN)// Other time pattern
                         .disableHtmlEscaping()
                         .setObjectToNumberStrategy(OBJECT_TO_NUMBER)
                         .create();
@@ -78,8 +81,9 @@ public class JsonUtils {
     private static class SingletonExposedHolder {
         private static final Gson GSON_INSTANCE =
                 new GsonBuilder()
-                        .setDateFormat(DateUtils.DATE_TIME_PATTERN)
                         .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+                        .registerTypeAdapter(Instant.class, new InstantTypeAdapter())
+                        .setDateFormat(DateUtils.DATE_TIME_PATTERN)// Other time pattern
                         .disableHtmlEscaping()
                         .excludeFieldsWithoutExposeAnnotation()
                         .setObjectToNumberStrategy(OBJECT_TO_NUMBER)
